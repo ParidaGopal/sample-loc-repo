85edfe67ffc72df0b26bf8de9d3daa30fbab418f
Parida,Gopal <gopal.parida@philips.com>
2023-02-22 15:31:22 +0530

    update 1

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const cors = require('cors');
 
+app.use(cors({
+    origin: '*'
+}));
  
 app.get('/api/test', (req,res)=> {
 res.send("Node api is working ..");
@@ -10,8 +14,15 @@ res.send("Node api is working ..");
 
 // http://localhost:3000/locales/en/translation.json
 app.get('/api/locales/:ln',(req,res) =>{
-    console.log(req.params.ln);
-    res.send('{"app_name":"Grootbasket"}');
+    console.log(`api hit with lang ${req.params.ln}`);
+
+    if(req.params.ln === 'en'){
+        res.send('{"app_name":"Grootbasket"}');
+    }
+    else {
+        res.send('{"app_name":"جروتباسكت"}');
+    }
+
 })
 
 
--- src/i18n.js
+++ src/i18n.js
@@ -13,10 +13,29 @@ i18next
   .init({
     lng: 'ar',
     debug: true,
+    defaultNS: "translation",
+    ns: 'translation',
     interpolation: {
       escapeValue: false,
     },
+    fallbackLng: 'en',
     supportedLngs: ["en", "ar"],
+    backend: {
+      //loadPath: 'locales/{{ns}}/{{lng}}.json',
+      loadPath: 'http://localhost:8080/api/locales/{{lng}}',
+      parse: (data) => { 
+        console.log(JSON.parse(data)); 
+        return JSON.parse(data); 
+      },
+      // customHeaders: {
+      //   "Access-Control-Allow-Origin": "*"
+      // },
+      requestOptions: { 
+        mode: 'cors',
+        credentials: 'same-origin',
+        cache: 'default'
+      }
+    },
   });
 
 export default i18next;
